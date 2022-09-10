463845d97284b1e83451cd11b49be9a689d020c0
jqzy91 <jamesquek91@live.com.sg>
2022-09-11 00:57:34 +0800

    portfolio page update, refined projects section, overall page refined

--- interactions.js
+++ interactions.js
@@ -19,4 +19,39 @@ for (let i = 0; i < navLink.length; i++) {
 
 // video playback speed
 
-document.querySelector("video").playbackRate = 0.5;
+// document.querySelector("video").playbackRate = 0.5;
+
+// slider
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let allSlides = document.getElementsByClassName("slides");
+  let dots = document.getElementsByClassName("slider-dots");
+  if (n > allSlides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = allSlides.length;
+  }
+  for (i = 0; i < allSlides.length; i++) {
+    allSlides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  allSlides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
