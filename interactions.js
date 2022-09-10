7c6485e9cf97a2c28e8a6056ae08d9ad0473e1e2
jqzy91 <jamesquek91@live.com.sg>
2022-09-10 09:37:10 +0800

    refined html, css styles and interaction, js active state

---
+++ interactions.js
@@ -0,0 +1,18 @@
+// test alert
+
+// alert("working");
+
+// active state of anchor links
+
+let topNavItems = document.getElementById("top-nav-items");
+let navLink = topNavItems.getElementsByClassName("nav-link");
+
+for (let i = 0; i < navLink.length; i++) {
+  navLink[i].addEventListener("click", function () {
+    let current = document.getElementsByClassName("active");
+    if (current.length > 0) {
+      current[0].className = current[0].className.replace(" active", "");
+    }
+    this.className += " active";
+  });
+}
