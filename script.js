43b89de097e7922e74c401d9b4acfaf29a83ab6c
ianthehamster <ianchow9898@gmail.com>
2023-10-03 21:28:04 +0800

    Oops forgot to commit my changes

---
+++ script.js
@@ -0,0 +1,29 @@
+function toggleMenu() {
+  const navLinks = document.querySelector(".nav-links");
+  navLinks.classList.toggle("show-menu");
+}
+
+const menuIcon = document.querySelector(".menu-icon");
+
+// Show/hide menu on click
+menuIcon.addEventListener("click", toggleMenu);
+
+// Hide menu when a link is clicked (optional)
+const navLinks = document.querySelectorAll(".nav-links a");
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    const navLinks = document.querySelector(".nav-links");
+    navLinks.classList.remove("show-menu");
+  });
+});
+
+// Show menu icon on smaller screens
+window.addEventListener("resize", () => {
+  if (window.innerWidth <= 768) {
+    menuIcon.style.display = "block";
+  } else {
+    menuIcon.style.display = "none";
+    const navLinks = document.querySelector(".nav-links");
+    navLinks.classList.remove("show-menu");
+  }
+});
