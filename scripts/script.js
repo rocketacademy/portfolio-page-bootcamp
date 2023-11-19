9455f804a0c3839e4e3803fe4c5a0683e72e54d9
Chxshire22 <akuindra22@gmail.com>
2023-11-19 14:18:14 +0800

    about modal complete, with fade in transition

--- scripts/script.js
+++ scripts/script.js
@@ -7,3 +7,21 @@ let toggleNav = () =>{
 const toggleBtn = document.getElementById("nav-toggle")
 
 toggleBtn.addEventListener("click",toggleNav)
+
+
+// modals
+
+const openAboutModal = document.querySelector(".about-link");
+const closeModal = document.querySelector(".modalClose");
+const aboutModal = document.querySelector(".modal-about");
+
+openAboutModal.addEventListener("click", ()=>{
+  aboutModal.showModal()
+  aboutModal.classList.add("show");
+  console.log("showModal Clicked");
+});
+closeModal.addEventListener("click", ()=>{
+  aboutModal.close();
+  aboutModal.classList.remove("show")
+  console.log("closeModal Clicked");
+})
